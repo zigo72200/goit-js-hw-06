06200f3303d4ff6770f3ce2d70e894ea20973c42
Viktor <zigo72200@gmail.com>
2022-02-20 18:46:32 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
 
+
+const refForm = document.querySelector('.login-form')
+
+refForm.addEventListener('submit', pushForm)
+
+
+
+function pushForm(submit) {
+    submit.preventDefault();
+
+    const formData = new FormData(submit.currentTarget)
+
+    const { email, password } = submit.currentTarget
+    
+    if (email.value === '' || password.value === '') {
+        alert('Потрібно заповнити всі поля')
+        return
+    }
+    
+    const authorization = {}
+    
+     formData.forEach((value, name) => {
+        
+         aaa[name] = value
+
+     })
+    
+    console.log(authorization)
+    
+
+        submit.currentTarget.elements.email.value = '';
+        submit.currentTarget.elements.password.value = '';
+    
+
+}
+
