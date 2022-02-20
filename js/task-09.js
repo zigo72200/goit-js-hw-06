b58f1b2a7018db75f2451faf6dcb517bd550045d
Viktor <zigo72200@gmail.com>
2022-02-20 19:51:11 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refButton = document.querySelector('.change-color');
+const refSpan = document.querySelector('.color');
+const refBody = document.querySelector('body')
+
+refButton.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor()
+  refBody.style.backgroundColor = color;
+  refSpan.textContent = color;
+}
